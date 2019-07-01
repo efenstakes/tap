b564d5b684ad1aae5fef8ad52dc9793649ab1cd1
= <efenstakes101@gmail.com>
2019-07-01 23:04:02 +0300

    add service request offers tables to the database schema so service providers can 'bid' for requests

---
+++ server/app/routes/service_requests.js
@@ -0,0 +1,16 @@
+
+
+// *** add a service provider jwt authentication mechanism
+
+// make a new one,
+
+// make a service request offer if service provider
+
+// delete 
+
+// chose service provider for a request -- done by the request maker
+
+// service provider to reject a request
+
+// add request rating after its done
+
--- server/db.sql
+++ server/db.sql
@@ -4,6 +4,7 @@
 drop database if exists `tap`;
 create database `tap`;
 
+use `tap`;
 
 -- keep user data
 create table `users` (
@@ -15,6 +16,7 @@ create table `users` (
    `city` varchar(50),
    `dob` date not null,
    `verified` boolean default false,
+   `min_charge` decimal(10, 2) default 0.0,
    `joined_on` datetime default NOW(),
    `user_type` enum('SERVICE_PROVIDER', 'USER', 'ADMIN') default 'USER',
    `is_company` boolean default false,
@@ -35,7 +37,20 @@ create table `service_requests`(
    `is_accepted` boolean default false,
 
    `made_on` datetime default NOW(),
-   foreign key(service_provider) references users(id)
+   foreign key(service_provider) references users(id),
    foreign key(made_by) references users(id),
    primary key(id)
+);
+
+
+-- allow service providers to try bid for a service request
+create table `request_offers` (
+  `id` int auto_increment,
+  `service_provider` int not null,
+  `service_request` int not null,
+  `description` longtext, 
+
+  foreign key(service_provider) references users(id),
+  foreign key(service_request) references service_requests(id), 
+  primary key(id)
 );
